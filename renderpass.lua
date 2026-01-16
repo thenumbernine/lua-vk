75f2760935aca7f89cafc362a484251e7d794e45
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-17 01:20:55 +0700

    more

---
+++ renderpass.lua
@@ -0,0 +1,85 @@
+require 'ext.gc'
+local ffi = require 'ffi'
+local class = require 'ext.class'
+local assert = require 'ext.assert'
+local vk = require 'vk'
+local vkassert = require 'vk.util'.vkassert
+local vkGet = require 'vk.util'.vkGet
+local makeStructCtor = require 'vk.util'.makeStructCtor
+
+
+local VkRenderPass = ffi.typeof'VkRenderPass'
+
+local makeVkSubpassDescription = makeStructCtor(
+	'VkSubpassDescription',
+	{
+		{
+			name = 'colorAttachments',
+			type = 'VkAttachmentReference',
+		},
+		{
+			-- NOTICE there's no matching Count field for this ...
+			-- ... should it always match colorAttachmentCount ?
+			name = 'resolveAttachments',
+			type = 'VkAttachmentReference',
+		},
+		{
+			notarray = true,	-- because most the time it's an array I'm permuting
+			name = 'depthStencilAttachment',
+			ptrname = 'pDepthStencilAttachment',
+			type = 'VkAttachmentReference',
+		},
+	},
+	true	-- no sType
+)
+
+
+local makeVkRenderPassCreateInfo = makeStructCtor(
+	'VkRenderPassCreateInfo',
+	{
+		{
+			name = 'attachments',
+			type = 'VkAttachmentDescription',
+		},
+		{
+			name = 'subpasses',
+			ptrname = 'pSubpasses',
+			countname = 'subpassCount',
+			type = 'VkSubpassDescription',
+			gen = makeVkSubpassDescription,
+		},
+		{
+			name = 'dependencies',
+			ptrname = 'pDependencies',
+			countname = 'dependencyCount',
+			type = 'VkSubpassDependency',
+		},
+	}
+)
+
+local VKRenderPass = class()
+
+function VKRenderPass:init(args)
+	self.device = assert.index(args, 'device')
+	args.device = nil
+
+	self.id = vkGet(
+		VkRenderPass,
+		vkassert,
+		vk.vkCreateRenderPass,
+		self.device,
+		makeVkRenderPassCreateInfo(args),
+		nil
+	)
+end
+
+function VKRenderPass:destroy()
+	if self.id then
+		vk.vkDestroyRenderPass(self.device, self.id, nil)
+	end
+	self.id = nil
+end
+
+VKRenderPass.__gc = VKRenderPass.destroy
+
+return VKRenderPass
